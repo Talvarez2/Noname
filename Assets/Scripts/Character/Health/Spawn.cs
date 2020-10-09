e51e394c650688675568fd700ad67125887602e1
Tomas <talvarez2@uc.cl>
2020-10-09 14:18:13 -0300

    :movie_camera: camera follows characters

---
+++ Assets/Scripts/Character/Health/Spawn.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawn : MonoBehaviour
+{
+    private CharacterController player;
+
+    public void spawn(GameObject character)
+    {
+        player = character.GetComponent<CharacterController>();
+        Debug.Log("respawn");
+        player.enabled = false;
+        player.transform.position = transform.position;
+        player.enabled = true;
+    }
+}
