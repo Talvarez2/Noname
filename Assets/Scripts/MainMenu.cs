2dc473d8f81979da8e77147c6e376c22fda99c11
nrfreudenberg <nrfreudenberg@uc.cl>
2020-10-12 22:50:06 -0300

    add music and options to change volume in main menu

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -4,6 +4,18 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
+
+    private AudioSource audioSource;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void changeAudioVolume(float volume)
+    {
+        audioSource.volume = volume;
+    }
     public void doExitGame()
     {
         Application.Quit();
