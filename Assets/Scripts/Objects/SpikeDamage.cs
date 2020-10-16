879bbf228c205dcb886ffd007823612d42f77f10
MatiasRojasUC <68394846+MatiasRojasUC@users.noreply.github.com>
2020-10-16 17:38:02 -0300

    Small Changes

---
+++ Assets/Scripts/Objects/SpikeDamage.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeDamage : MonoBehaviour
+{
+    public int damage = 10;
+    BoxCollider m_Collider;
+
+    private void Start() 
+    {
+        //Fetch the Collider from the GameObject
+        m_Collider = GetComponent<BoxCollider>();
+        m_Collider.size = new Vector3(1, 5, 1);
+        m_Collider.isTrigger = true;
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<HealthAndDamage>().InflictDamage(damage);
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+        if (other.tag == "Player")
+        {
+            other.GetComponent<HealthAndDamage>().InflictDamage(damage);
+        }
+    }
+}
