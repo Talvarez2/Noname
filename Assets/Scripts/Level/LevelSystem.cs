383eb6dcd77d4e346bd79eaf1c292569613bc713
nrfreudenberg <nrfreudenberg@uc.cl>
2020-11-03 17:44:40 -0300

    next level button in win scene working

---
+++ Assets/Scripts/Level/LevelSystem.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSystem : MonoBehaviour
+{
+    private int implementedLevels = 2;
+
+    private void Start()
+    {
+        int c = SceneManager.sceneCount;
+        for (int i = 0; i < c; i++)
+        {
+            Scene s = SceneManager.GetSceneAt(i);
+            if (s.name.Contains("Level"))
+            {
+                Scene activeScene = SceneManager.GetActiveScene();
+                if (s != activeScene)
+                {
+                    SceneManager.SetActiveScene(s);
+                }
+            }
+        }
+    }
+
+    public int GetActualLevel()
+    {
+        Scene s = SceneManager.GetActiveScene();
+        int actualLevel = Int32.Parse(s.name.Substring(s.name.Length - 1));
+        return actualLevel;
+    }
+
+    public bool CheckNextLevel()
+    {
+        if (GetActualLevel() < implementedLevels)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+}
