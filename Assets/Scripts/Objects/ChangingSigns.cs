1f991632a37e8275d5ec5071d2427c995faf1e7b
Tomas <talvarez2@uc.cl>
2020-11-24 17:03:37 -0300

    :rocketready

---
+++ Assets/Scripts/Objects/ChangingSigns.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangingSigns : MonoBehaviour
+{
+    public Sprite SkullPanel;
+    public Sprite HeartPanel;
+    public bool Damage;
+    public bool Change;
+    private SpriteRenderer spriteRenderer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        SetSprite();
+    }
+
+    void SetSprite()
+    {
+        if (Damage){
+            spriteRenderer.sprite = SkullPanel;
+        } else {
+            spriteRenderer.sprite = HeartPanel;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other) {
+        if (Change)
+        {
+            Change = false;
+            Damage = !Damage;
+            SetSprite();
+        }
+    }
+
+}
