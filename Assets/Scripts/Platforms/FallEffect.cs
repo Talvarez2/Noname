eea6c44e8fa9e4708c31251b4818bd7746bc30a9
talvarez2 <talvarez2@uc.cl>
2020-12-16 13:21:08 -0300

    :bug: fix disapearing falling platforms

--- Assets/Scripts/Platforms/FallEffect.cs
+++ Assets/Scripts/Platforms/FallEffect.cs
@@ -5,16 +5,22 @@ using UnityEngine;
 public class FallEffect : MonoBehaviour
 {
     public int timeBeforeFall = 5;
-    public int timeBeforeDestroy = 1;
+    public int timeBeforeDestroy = 2;
+    public int timeBeforeReapear = 10;
     BoxCollider m_Collider;
+    MeshRenderer m_Renderer;
     Rigidbody rigidBody;
     private MeshRenderer meshRenderer;
+    private Vector3 startPostition;
+    private bool shuffleColor = true;
 
     private void Start() 
     {
         // Fetch the Collider from the GameObject
         m_Collider = GetComponent<BoxCollider>();
+        m_Renderer = GetComponent<MeshRenderer>();
         BoxCollider boxCollider = gameObject.AddComponent<BoxCollider>();
+        startPostition = this.transform.position;
         rigidBody = gameObject.AddComponent<Rigidbody>();
         rigidBody.isKinematic = true;
         rigidBody.useGravity = false;
@@ -27,14 +33,27 @@ public class FallEffect : MonoBehaviour
         if (other.tag == "Player")
         {
             StartCoroutine(ToogleImageSize());
+            shuffleColor = true;
             StartCoroutine(Fall());
             StartCoroutine(Destroy());
         }
     }
 
+    private void restart(){
+        rigidBody.useGravity = false;
+        rigidBody.isKinematic = true;
+        m_Renderer.enabled = true;
+        m_Collider.enabled = true;
+        this.transform.position = startPostition;
+        shuffleColor = false;
+        meshRenderer.material.color = new Color(1, 1, 1, 0_01f);
+
+    }
+
     IEnumerator Fall()
     {
         // wait and then fall
+        Debug.Log("fall");
         yield return new WaitForSeconds(timeBeforeFall);
         rigidBody.useGravity = true;
         rigidBody.isKinematic = false;
@@ -42,7 +61,7 @@ public class FallEffect : MonoBehaviour
 
     IEnumerator ToogleImageSize()
     {
-        while (true)
+        while (shuffleColor)
         {
             meshRenderer.material.color = Color.red;
             yield return new WaitForSeconds(0.2F);
@@ -55,6 +74,10 @@ public class FallEffect : MonoBehaviour
     {
         // wait and then destroy
         yield return new WaitForSeconds(timeBeforeFall+timeBeforeDestroy);
-        Destroy(this.gameObject);
+        m_Renderer.enabled = false;
+        m_Collider.enabled = false;
+        // Destroy(this.gameObject);
+        yield return new WaitForSeconds(timeBeforeFall+timeBeforeDestroy+timeBeforeReapear);
+        restart();
     }
 }
