85e3e757bc175232a560163551ca6f214f504be4
MatiasRojasUC <68394846+MatiasRojasUC@users.noreply.github.com>
2020-12-17 18:58:38 -0300

    asd

--- Assets/Scripts/Menus/MainMenu.cs
+++ Assets/Scripts/Menus/MainMenu.cs
@@ -7,6 +7,7 @@ public class MainMenu : MonoBehaviour
 {
 
     private AudioSource audioSource;
+    private AudioSource gameaudioSource;
     private string standardLevelSceneName = "Level-";
     private string pauseSceneName = "Pause";
     private string lostSceneName = "YouLost";
@@ -15,19 +16,34 @@ public class MainMenu : MonoBehaviour
     void Start()
     {
         float volume = PlayerPrefs.GetFloat("Music Volume", 1);
+        float gamevolume = PlayerPrefs.GetFloat("Game Volume", 1);
 
         audioSource = GetComponent<AudioSource>();
         audioSource.volume = volume;
 
+        gameaudioSource = GetComponent<AudioSource>();
+        gameaudioSource.volume = gamevolume;
+
         GameObject temp = this.transform.Find("OptionsMenu").gameObject;
+
         temp = temp.transform.Find("MusicVolumeSlider").gameObject;
         temp.GetComponent<Slider>().normalizedValue = volume;
+
+        GameObject temp2 = this.transform.Find("OptionsMenu").gameObject;
+        temp2 = temp.transform.Find("GameVolumeSlider").gameObject;
+        temp2.GetComponent<Slider>().normalizedValue = gamevolume;
     }
 
     public void changeAudioVolume(float volume)
     {
         audioSource.volume = volume;
         PlayerPrefs.SetFloat("Music Volume", volume);
+    }
+
+    public void changeGameVolume(float gamevolume)
+    {
+        gameaudioSource.volume = gamevolume;
+        PlayerPrefs.SetFloat("Game Volume", gamevolume);
     }
 
     public void changeToLevel(int levelNumber)
