a7e1a79296ae84d6bd5d1833ebb03ec6162d82ef
MatiasRojasUC <68394846+MatiasRojasUC@users.noreply.github.com>
2020-12-17 22:34:03 -0300

    checkpoints well placed and with textures

--- Assets/Scripts/Objects/SpawnPoint.cs
+++ Assets/Scripts/Objects/SpawnPoint.cs
@@ -7,11 +7,25 @@ public class SpawnPoint : MonoBehaviour
     private Color blue = Color.blue;
     private Color red = Color.red;
     private Color green = Color.green;
+    private GameObject Light;
     private Light _light;
+    private SpriteRenderer spriteRed;
+    private SpriteRenderer spriteGreen;
+    private SpriteRenderer spriteBlue;
+
 
     void Start()
     {
         _light = gameObject.GetComponentInChildren<Light>();
+        
+
+        spriteRed = this.transform.Find("SpriteRed").GetComponent<SpriteRenderer>();
+        spriteGreen = this.transform.Find("SpriteGreen").GetComponent<SpriteRenderer>();
+        spriteBlue = this.transform.Find("SpriteBlue").GetComponent<SpriteRenderer>();
+
+        spriteRed.enabled = false;
+        spriteGreen.enabled = false;
+        spriteBlue.enabled = true;
     }
 
     public void ChangeColor(string color)
@@ -20,14 +34,23 @@ public class SpawnPoint : MonoBehaviour
         if (color == "red")
         {
             _light.color = red;
+            spriteRed.enabled = true;
+            spriteGreen.enabled = false;
+            spriteBlue.enabled = false;
         }
         else if (color == "blue")
         {
             _light.color = blue;
+            spriteRed.enabled = false;
+            spriteGreen.enabled = false;
+            spriteBlue.enabled = true;
         }
         else if (color == "green")
         {
             _light.color = green;
+            spriteRed.enabled = false;
+            spriteGreen.enabled = true;
+            spriteBlue.enabled = false;
         }
     }
 
